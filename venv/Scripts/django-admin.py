604d2e84abe355f8901d7257cc221a1f5c9a926d
BorisVV <bcarlof@gmail.com>
2017-04-05 22:47:09 -0500

    fixed gitignore issue

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\borisv~1\desktop\capsto~1\labs\lab8\django~1\myfirs~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
