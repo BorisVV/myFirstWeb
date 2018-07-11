ac80f48840d714da7538404badf8e8659387edf0
BorisVV <bcarlof@gmail.com>
2018-07-11 00:16:07 -0500

    Re-wrote the model for the user/users

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import UserManager
 
-class CustomUser(AbstractUser):
-    def __str__(self):
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_('username'), max_length=30, unique=True)
+    email = models.EmailField(_('email address'), unique=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_username(self):
         return self.username
 
 class Post(models.Model):
-    author_name = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    author_name = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
