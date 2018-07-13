c3eb8ebfd4a2b1ab4e4d31516ae297e580b08ff4
BorisVV <bcarlof@gmail.com>
2018-07-13 11:49:52 -0500

    adding user issues

--- blog/managers.py
+++ blog/managers.py
@@ -7,24 +7,22 @@ class UserManager(BaseUserManager):
         """
         Creates and saves a User with the given email and password.
         """
-        if not username:
-            raise ValueError('The given username must be set')
-        elif not email:
+        if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, mail, password, **extra_fields)
+        return self._create_user(mail, password, **extra_fields)
 
-    def create_superuser(self, username, email, password, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(username, mail, password, **extra_fields)
+        return self._create_user(mail, password, **extra_fields)
