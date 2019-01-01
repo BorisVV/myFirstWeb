e7c31ed42b171fe2c784850f1f2572227ae15b57
BorisVV <bcarlof@gmail.com>
2019-01-01 08:15:53 -0600

    Added new examples of JQuery.

--- blog/static/js/blog.js
+++ blog/static/js/blog.js
@@ -22,3 +22,51 @@ function setCSS() {
   $('p:nth-child(1)').css("font-style", "italic")
   $('p:nth-child(3)').css("font-style", "italic")
 }
+
+function myFunction() {
+  $('#addClasses').addClass("redClass");
+}
+
+function showText() {
+  $(".divMsgShow").show();
+}
+function hideText() {
+  $(".divMsgHide").hide();
+}
+function toggleEffect() {
+  $(".divMsgToggle").toggle();
+}
+function slideUp() {
+  $(".divMsgSlideUp").slideUp();
+}
+function slideDown() {
+  $(".divMsgSlideDown").slideDown();
+}
+
+function insertBeforeHtml() {
+  $("<div style='color:green;'>This text is inserted by insertBefore() method.</div>").insertBefore($("#divinsertBefore"));
+}
+
+function checkGenger() {
+  if($("#inputChkMale").is(":checked")) {
+    alert("Male is selected");
+  }
+  else if ($("#inputChkFemale").is(":checked")) {
+    alert("Female is selected");
+  }
+}
+
+// .replaceWith()
+function replaceWith() {
+  $('#replaceWith').replaceWith("<p style='color:green;'>Hello, this is a new text!</p>")
+}
+
+// number of boxes checked.
+function getChkdCount() {
+  alert($("input:checked").length);
+}
+
+// get the selected value.
+function getSelectedValue(){
+  alert($("select option:selected").text());
+}
